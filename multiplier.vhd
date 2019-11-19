a54df44acd9fdf249469fc54c960ad4efdb3822d
Pedro Javier <pedro-javierf@users.noreply.github.com>
2019-11-19 12:56:31 +0100

    Add files via upload

--- multiplier.vhd
+++ multiplier.vhd
@@ -3,14 +3,9 @@ use ieee.std_logic_1164.all;
 use work.definitions.all;
 
 entity ASM_multiplier is
-    port( reset, CLOCK_24, init : in  std_logic;
-          SW               : in  std_logic_vector(9 downto 0);
-          done             : inout std_logic;
-			 LEDR					: out std_logic_vector(9 downto 0);
-			 LEDG 				: out std_logic_vector(7 downto 0);
-			 KEY					: in std_logic_vector(3 downto 0);
-			 HEX0 				: out std_logic_vector(6 downto 0);
-			 HEX1 				: out std_logic_vector(6 downto 0);
+    port( reset, clk, init : in  std_logic;
+          a_in, b_in       : in  std_logic_vector(W_FACTORS-1 downto 0);
+          done             : out std_logic;
           r                : out std_logic_vector(W_RESULT-1  downto 0) );
 end ASM_multiplier;
 
@@ -30,29 +25,34 @@ architecture arch_ASM_mult of ASM_multiplier is
               status     : out std_logic_vector(W_STATUS-1  downto 0);
               r          : out std_logic_vector(W_RESULT-1  downto 0) );
     end component data_path;
-	 
-	 component conv_7seg
+    
+    component displays
+    Port ( 
+        rst : in STD_LOGIC;
+        clk : in STD_LOGIC; 
+        velocidad : in  STD_LOGIC_VECTOR (1 downto 0);      
+        display : out  STD_LOGIC_VECTOR (6 downto 0);
+        display_enable : out  STD_LOGIC_VECTOR (3 downto 0)
+     );
+     end component displays;
+
+    component conv_7seg
     Port ( x : in  STD_LOGIC_VECTOR (3 downto 0);
            display : out  STD_LOGIC_VECTOR (6 downto 0));
-	 end component conv_7seg;
+end component conv_7seg;
 
     signal status  : std_logic_vector(W_STATUS-1  downto 0);
     signal control : std_logic_vector(W_CONTROL-1 downto 0);
-	 signal output  : std_logic_vector(W_RESULT-1  downto 0);
+    signal display : std_logic_vector(6 downto 0);
 begin
-	
-	 LEDG(0) <= done;
-	 LEDR(7 downto 0) <= output;
-	
-	 --HEX0 <= 
-	
+
     U_CNTRL: controller
-        port map(CLOCK_24, not(KEY(0)), not(KEY(3)), status, control, done);
+        port map(clk, reset, init, status, control, done);
 
     U_DP: data_path
-        port map(CLOCK_24, not(KEY(0)), SW(3 downto 0), SW(7 downto 4), control, status, output);
-		  
-	 LCD_1: conv_7seg port map(output(3 downto 0), HEX0);
-	 LCD_2: conv_7seg port map(output(7 downto 4), HEX1);
+        port map(clk, reset, a_in, b_in, control, status, r);
+
+    DSP: displays port map(reset, clk, "11", display, "1100");
+    
 
 end arch_ASM_mult;
